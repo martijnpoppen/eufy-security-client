64b632a6c369d8f67c2654040de4149a8547ebc1
Martijn Poppen <m.poppen@ziggo.nl>
2023-09-01 11:42:00 +0200

    OPT: build

--- build/http/parameter.d.ts
+++
@@ -1,5 +0,0 @@
-import { Logger } from "ts-log";
-export declare class ParameterHelper {
-    static readValue(type: number, value: string, log: Logger): string | undefined;
-    static writeValue(type: number, value: string): string;
-}
